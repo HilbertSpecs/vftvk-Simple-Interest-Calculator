55a37759468db4b5a515c6c4463cddd794de03cb
Evan Rabeaux <evanjames1990@gmail.com>
2021-11-11 01:43:25 +0000

    Validate User Input Principal

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function compute()
 {
+    validatePrincipal();
+
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
